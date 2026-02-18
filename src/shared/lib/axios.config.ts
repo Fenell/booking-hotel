c82741a198611b1e5d08f00576dd92a9a66cfed7
tuandat <maituandat087804@gmail.com>
2026-02-18 14:24:38 +0700

    add token default header axios

--- src/shared/lib/axios.config.ts
+++ src/shared/lib/axios.config.ts
@@ -7,16 +7,41 @@ const axiosInstance = axios.create({
 });
 
 // Request interceptor
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("access_token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const { response } = error;
+
+    if (response) {
+      // handleErrorResponse(response);
+
+      if (response.status === 401) {
+        // document.cookie =
+        //   "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        window.location.href = "/login";
+      }
+    } else if (error.request) {
+      // showErrorToast("NETWORK_ERROR");
+    } else {
+      // showErrorToast("DEFAULT_ERROR");
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 // // Response interceptor
 // axiosInstance.interceptors.response.use(
